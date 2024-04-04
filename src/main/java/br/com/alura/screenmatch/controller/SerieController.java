3137134ef10bfa10bdba6f4a062f084899dea983
nicolasdca <nicolasdavid193@gmail.com>
2024-04-03 22:50:22 -0300

    Aula 2 e 3 Spring Web

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,12 +1,41 @@
 package br.com.alura.screenmatch.controller;
 
+import br.com.alura.screenmatch.dto.SerieDTO;
+import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.service.SerieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/series")
 public class SerieController {
-    @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui vão ser listadas as séries";
+
+    @Autowired
+    SerieService servico;
+    @GetMapping
+    public List<SerieDTO> obterSeries() {
+        return servico.obterTodasAsSeries();
     }
+
+    @GetMapping("/top5")
+    public List<SerieDTO> obterTop5Series() {
+        return servico.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterSeriesLancamento() {
+        return servico.obterSeriesLancamento();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return servico.obterPorId(id);
+    }
+
+
 }
